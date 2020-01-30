428a8448b63682aaafb035c130ec15ec38281984
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-01-31 00:46:43 +0100

    solution

--- Bear and Big Brother.py
+++ Bear and Big Brother.py
@@ -1,8 +1,13 @@
 from sys import stdin, stdout
 
-n, m = map(int, stdin.readline().split())
-output = 0
-while n <= m:
-    n, m = n * 3, m * 2
-    output += 1
-stdout.write(str(output))
+
+def main():
+    n, m = map(int, stdin.readline().split())
+    output = 0
+    while n <= m:
+        n, m = n * 3, m * 2
+        output += 1
+    stdout.write(str(output))
+
+
+main()
--- Hulk.py
+++ Hulk.py
@@ -1,1 +1,4 @@
-n = int(input())
+from sys import stdin, stdout
+
+n = int(stdin.readline())
+
