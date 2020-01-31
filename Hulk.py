122d658dda99849bc10b1abb4058c357841297ab
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-01-31 01:05:13 +0100

    solution

--- Hulk.py
+++ Hulk.py
@@ -1,4 +1,14 @@
 from sys import stdin, stdout
 
 n = int(stdin.readline())
-
+prob = ['I'] * (n+1)
+for i in range(n-1):
+    if i % 2:
+        prob[i+1] = 'love that'
+    else:
+        prob[i+1] = 'hate that'
+if n % 2:
+    prob[-1] = 'hate it'
+else:
+    prob[-1] = 'love it'
+stdout.write(' '.join(prob))
