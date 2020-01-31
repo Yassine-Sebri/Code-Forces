da31d31058fc74130befc263aed4d91fedd3024b
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-01-31 02:56:15 +0100

    solution

--- Twins.py
+++ Twins.py
@@ -1,10 +1,15 @@
-def main():
-    n = input()
-    prob = sorted([int(number) for number in input().split()], reverse=True)
+def solution(prob):
     index = 1
     while sum(prob[:index]) <= sum(prob[index:]):
-        index += 1
+            index += 1
     print(len(prob[:index]))
 
 
-main()
+def main():
+    n = input()
+    prob = sorted([int(number) for number in input().split()], reverse=True)
+    solution(prob)
+
+
+if __name__ == '__main__':
+    main()
