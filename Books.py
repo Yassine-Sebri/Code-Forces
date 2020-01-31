5aa065a87bb0d2b57c589f5b4b94cb5ee7b20fb0
Yassine-Sebri <yassine.sebri@enetcom.u-sfax.tn>
2020-01-31 06:58:49 +0100

    solution

--- Books.py
+++ Books.py
@@ -3,13 +3,16 @@ def main():
     books = [int(number) for number in input().split()]
     maximum = 0
     for i in range(n):
-        index, curr, time = i, 0, 0
-        while index < n and (time + books[index]) <= t:
-            time += books[index]
-            index += 1
-            curr += 1
-        if curr > maximum:
-            maximum = curr
+        pointer1 = i
+        if pointer1 > 0:
+            time = time - books[pointer1 - 1]
+        else:
+            pointer2, time = 0, 0
+        while pointer2 < n and (time + books[pointer2]) <= t:
+            time += books[pointer2]
+            pointer2 += 1
+        if pointer2 - pointer1 > maximum:
+            maximum = pointer2 - pointer1
     print(maximum)
 
 
